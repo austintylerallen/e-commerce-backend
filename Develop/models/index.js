d1e1974c040ff2f4d074f058a4df41e9d0b8067e
austinallen <austintallen07@gmail.com>
2024-05-21 00:19:15 -0400

    finally finished indexfor models/index.js. working on the category-routes.js that I can't get working yet

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,50 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const Category = require('./Category');
+const Product = require('./Product');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-class Product extends Model {}
+// Product belongs to Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-Product.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true,
-      },
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true,
-      },
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+// Category has many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-module.exports = Product;
+// Product belongs to many Tags through ProductTag
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// Tag belongs to many Products through ProductTag
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Category,
+  Product,
+  Tag,
+  ProductTag,
+};
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,7 +3,6 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-// GET all categories
 router.get('/', async (req, res) => {
   try {
     const categories = await Category.findAll({
@@ -15,64 +14,58 @@ router.get('/', async (req, res) => {
   }
 });
 
-// GET a single category by id
 router.get('/:id', async (req, res) => {
   try {
     const category = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
-
     if (!category) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-
     res.status(200).json(category);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// POST a new category
 router.post('/', async (req, res) => {
   try {
-    const category = await Category.create(req.body);
-    res.status(200).json(category);
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-// PUT update a category by id
 router.put('/:id', async (req, res) => {
   try {
     const category = await Category.update(req.body, {
-      where: { id: req.params.id },
+      where: {
+        id: req.params.id,
+      },
     });
-
     if (!category) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-
     res.status(200).json(category);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-// DELETE a category by id
 router.delete('/:id', async (req, res) => {
   try {
     const category = await Category.destroy({
-      where: { id: req.params.id },
+      where: {
+        id: req.params.id,
+      },
     });
-
     if (!category) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-
     res.status(200).json(category);
   } catch (err) {
     res.status(500).json(err);
